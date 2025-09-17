231674c7bfd0b793d1422bc64756ec01f0b2ef72
Julyana Gusmão <julyanagcneiva@gmail.com>
2025-09-17 15:58:35 -0300

    comment cv

--- app/[locale]/sections/AboutMe.tsx
+++ app/[locale]/sections/AboutMe.tsx
@@ -46,8 +46,8 @@ const AboutMe = () => {
               </span>
             </a>
             <a
-              href="/Curriculo-Julyana-Gusmao.pdf"
-              download
+              href="#"
+              // download
               className="px-1 inline-block py-1 w-full rounded-full bg-gradient-to-br from-linear-l to-linear-r hover:bg-slate-800 text-base cursor-pointer transition-all duration-100 ease-in transform hover:scale-105"
             >
               <span className="block bg-main-color hover:bg-slate-800 rounded-full py-2 cursor-pointer transition-all duration-100 ease-in transform hover:scale-100">
@@ -103,8 +103,8 @@ const AboutMe = () => {
               </span>
             </a>
             <a
-              href="/Curriculo-Julyana-Gusmao.pdf"
-              download
+              href="#"
+              // download
               className="px-1 inline-block py-1 w-full rounded-full bg-gradient-to-br from-linear-l to-linear-r hover:bg-slate-800 mt-3 text-sm cursor-pointer transition-all duration-100 ease-in transform hover:scale-105"
             >
               <span className="block bg-main-color hover:bg-slate-800 rounded-full py-2 cursor-pointer transition-all duration-100 ease-in transform hover:scale-100">
